616951a400e560c8feb9b2cfea1667969414f038
SaayinathK <k.saayinath@gmail.com>
2026-09-25 14:43:11 +0530

    Move hardcoded Firebase credentials to environment variables

--- admin/src/pages/new/config.js
+++ admin/src/pages/new/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCTJYUotiKTJfPEMa32EdP6yBojOWjcltw",
-  authDomain: "upload-4649b.firebaseapp.com",
-  projectId: "upload-4649b",
-  storageBucket: "upload-4649b.appspot.com",
-  messagingSenderId: "1066914192254",
-  appId: "1:1066914192254:web:e0ab6d418b3b24d878f12f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
